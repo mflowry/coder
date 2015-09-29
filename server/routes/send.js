45c77d72e8dca0e9b029c8879fc5ba620354dab2
Madeleine Lowry <mlowry55403@gmail.com>
2015-09-29 09:51:30 -0500

    Contact Us form works Woo

--- server/routes/send.js
+++ server/routes/send.js
@@ -1,35 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
-var nodemailer = require('nodemailer');
-var mg = require('nodemailer-mailgun-transport');
+var request = require('request');
 
-var auth = {
-    auth: {
-        api_key: 'key-3188ca5ee2af760410a61c6e3772fc7a',
-        domain: 'https://api.mailgun.net/v3/sandboxf36386f5333e47e5b0875382c7ab6094.mailgun.org'
-    }
-};
-var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+var mailGunUrl = 'https://api:key-3188ca5ee2af760410a61c6e3772fc7a@api.mailgun.net/v3/sandboxf36386f5333e47e5b0875382c7ab6094.mailgun.org/messages';
 
 var to = 'swcoderdojo@gmail.com';
 
 /* Post volunteer page. */
 router.post('/', function(req, res, next) {
+    console.log('req.body:', req.body);
     var options = {
         from: req.body.fromEmail,
         to: to, // An array if you have multiple recipients.
         subject: 'Mail from ' + req.body.name + ': ' + req.body.subject,
         text: req.body.message
     };
+    console.log('options:', options);
 
-    nodemailerMailgun.sendMail(options, function (err, info) {
+    request.post(mailGunUrl, {form: options }, function (err, httpResponse, body) {
         if (err) {
+            console.log(err);
             next(err);
         }
         else {
-            console.log('Response: ' + info);
-            res.status(200).send('Email sent.');
+            console.log('Response: ', httpResponse.statusCode, httpResponse.statusMessage);
+            if (httpResponse.statusCode === 200) {
+                res.status(200).send('Email sent.');
+            } else {
+                res.status(500).send('There was an error processing your request. Try again later!');
+            }
         }
     });
 
