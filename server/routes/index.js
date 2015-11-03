b9f71b154892c12b7a0a2c0e8ee2af5edc9f8ff8
Madeleine Lowry <mlowry55403@gmail.com>
2015-11-03 09:08:36 -0600

    change date Nov

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,28 +7,9 @@ var app = express();
 router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
+router.get('/*', function(req,res,next){
+  res.redirect('/');
+});
 
-//router.get('/*', function(req,res,next){
-//  res.redirect('/');
-//});
-
-//app.get('/send.html',function(req,res){
-//  var mailOptions={
-//    name: req.body.name,
-//    fromEmail: req.body.fromEmail,
-//    subject : req.body.subject,
-//    message : req.body.message
-//  }
-//  console.log(mailOptions);
-//  smtpTransport.sendMail(mailOptions, function(error, response){
-//    if(error){
-//      console.log(error);
-//      res.end("error");
-//    }else{
-//      console.log("Message sent: " + response.message);
-//      res.end("sent");
-//    }
-//  });
-//});
 
 module.exports = router;
